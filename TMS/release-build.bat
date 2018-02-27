48c8064a511a51cd8bccd3dc4acf4d0cf98123a5
koala-v <1904579228@qq.com>
2018-02-27 14:21:39 +0800

    update release-build.bat

--- TMS/release-build.bat
+++ TMS/release-build.bat
@@ -8,7 +8,7 @@ cd platforms\android\build\outputs\apk
 
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../../../../my-release-key.keystore android-armv7-release-unsigned.apk alias_name
 
-C:\Android\sdk\android-sdk\build-tools\22.0.1\zipalign.exe -v 4 android-armv7-release-unsigned.apk Tan-Agent.apk
+F:\Android\sdk\android-sdk\build-tools\22.0.1\zipalign.exe -v 4 android-armv7-release-unsigned.apk Tan-Agent.apk
 
 xcopy /y "%~dp0platforms\android\build\outputs\apk\Tan-Agent.apk" "%~dp0"
 
